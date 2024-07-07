b3537fbe2993ec38b400bea4120eed6909484b4a
Young Eon Park <93845759+yepp0502@users.noreply.github.com>
2024-07-07 16:35:52 +0900

    commit home page stuff

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 export default function Home() {
-    
+    return (
+        <div className='home-container'>
+            <h1>portfolio</h1>
+            <div className='child-container'>
+                <Link to="/education" className='nav-el'>education</Link>
+                <Link to="/projects" className='nav-el'>projects</Link>
+                <Link to="/work-experience" className='nav-el'>work experience</Link>
+            </div>
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route element={<Header />}>
           <Route path="/education" element={<Education />} />
           <Route path="/projects" element={<Projects />} />
