e433c9da4bcd6f1aa5383cf655bd0afac7401e7f
Young Eon Park <93845759+yepp0502@users.noreply.github.com>
2024-07-07 16:12:51 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import Home from './Pages/Home'
+import Education from './Pages/Education'
+import Projects from './Pages/Projects'
+import WorkExperience from './Pages/WorkExperience'
+import Header from './Layout/Header'
+import RoomElement from './Layout/RoomElement';
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route element={<Header />}>
+          <Route path="/education" element={<Education />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/work-experience" element={<WorkExperience />} />
+        </Route>
+      </Routes>
+      <RoomElement />
+    </BrowserRouter>
+  );
+ }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
