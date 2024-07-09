aa59500e7639a71e987282ead5b128426649d940
Young Eon Park <93845759+yepp0502@users.noreply.github.com>
2024-07-09 16:07:03 +0900

    landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 export default function LandingPage() {
-    
+    return(
+        <div className='landing-container'>
+            <div className='main-text-container'>
+                <div>Hi, this is </div><br /><div id="beforenamediv"><span id='name'>Young Eon</span>'s</div><div>portfolio.</div>
+            </div>
+            <div className='continue-header'>
+                Click <span><Link to="/home">here</Link></span> to go!
+            </div>
+        </div>
+    );
 }
